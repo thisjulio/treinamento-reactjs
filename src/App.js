5848883d957ce27e919d662b94bae8c29875c1b2
Julio Cesar da Silva Pereira <julio@iMac-de-Julio.local>
2020-05-25 21:00:51 -0300

    v0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// `Caixa de edição`
+// Lista de Todo (`Lista de tarefa` [ul] > Exibe `tarefas` [li])
+// Lista de doing
+// Lista de Done
+
+export const CaixaEdicaoTexto = ({enviar}) => {
+  const inputRef = useRef();
+  return <div>
+    <input ref={inputRef} type="text" placeholder="Adicione o texto da tarefa..." />
+    <button onClick={()=>{
+      enviar(inputRef.current.value);
+      inputRef.current.value = "";
+    }}>Criar</button>
+  </div>
+};
+
+const Tarefa = ({texto}) => <li>{texto}</li>;
+
+export const ListaTarefas = ({tarefas = [], titulo}) => <>
+  <h1>{titulo}</h1>
+  <ul>
+    { tarefas.map( (textoTarefa, index) => <Tarefa key={index} texto={textoTarefa} /> ) }
+  </ul>
+</>;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CaixaEdicaoTexto />
+      <ListaTarefas tarefas={["Teste 1"]} titulo={} />
+      <h1>DOING</h1>
+      <ListaTarefas />
+      <h1>DONE</h1>
+      <ListaTarefas />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,43 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React, { useState } from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App, { ListaTarefas, CaixaEdicaoTexto } from './App';
 
-test('renders learn react link', () => {
+test('APP deve exibir lista de TODO, DOING e DONE', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  
+});
+
+test('Lista de tarefa deve exibir ul com as tarefas em li', () => { 
+  const { getByText } = render(<ListaTarefas titulo="TODO" tarefas={["Tarefa Todo de Exemplo"]} />);
+
+  const Element = getByText(/TODO/, {selector: "h1"});
+  const TaskItem = getByText("Tarefa Todo de Exemplo", {selector: "ul > li"});
+
+  expect(Element).toBeInTheDocument();
+  expect(TaskItem).toBeInTheDocument();
+});
+
+
+test('Caixa de edição deve exibir uma nova tarefa', () => { 
+  const MockComponent = () => {
+    const [mockState, setMockState] = useState(["Tarefa Todo de Exemplo"]);
+    return <>
+      <CaixaEdicaoTexto enviar={(text) => {
+        setMockState(p => [...p, text]);
+      }} />
+      <ListaTarefas titulo="TODO" tarefas={mockState} />
+    </>;
+  }
+  const { getByText, getByPlaceholderText } = render(<MockComponent />);
+
+  const inputElement = () => getByPlaceholderText("Adicione o texto da tarefa...");
+  const ButtonElement = getByText("Criar", {selector: "button"});
+  
+  fireEvent.change(inputElement(), { target: { value: "Tarefa de Teste" } });
+  fireEvent.click(ButtonElement);
+  
+  expect(inputElement().value).toBe("");
+  const TaskItem = getByText("Tarefa de Teste", {selector: "ul > li"});
+  expect(TaskItem).toBeInTheDocument();
 });
